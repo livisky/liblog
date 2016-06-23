931615a1faa18b2e1fad607013656050ca67efac
livisky <262248861@qq.com>
2016-06-23 19:23:46 +0800

    删除测试邮箱代码

--- src/common/config/mail.js
+++ src/common/config/mail.js
@@ -3,13 +3,13 @@
  * config
  */
 export default {
-  //发送邮件配置
-  //host:"smtp.qq.com",
-  //port: 465,
-  //domains:'["qq.com"]',
-  //account:'262248861@qq.com',
-  //pass: "wvebvwxcsxpebiai",
-  //from:"262248861@qq.com",
-  //route_on: true,
-  //encoding: "utf-8"
+  发送邮件配置
+  host:"smtp.qq.com",
+  port: 465,
+  domains:'["qq.com"]',
+  account:'262248861@qq.com',
+  pass: "1234567",
+  from:"262248861@qq.com",
+  route_on: true,
+  encoding: "utf-8"
 };
