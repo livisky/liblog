7caab74c81c8fdab9c657dc0644adea06cb45b61
livisky <262248861@qq.com>
2016-06-23 19:22:41 +0800

    删除测试邮箱代码

--- src/common/config/mail.js
+++ src/common/config/mail.js
@@ -12,12 +12,4 @@ export default {
   //from:"262248861@qq.com",
   //route_on: true,
   //encoding: "utf-8"
-  host:"hikml2.hikvision.com.cn",
-  port: 25,
-  domains:'["hikvision.com"]',
-  account:'ys_admin@hikvision.com.cn',
-  pass: "$Password01",
-  from:"ys_admin@hikvision.com.cn",
-  route_on: true,
-  encoding: "utf-8"
 };
