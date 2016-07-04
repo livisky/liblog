c0a7fe7d120167d33729246b3653a91ddfe816d5
livisky <262248861@qq.com>
2016-07-04 14:32:43 +0800

    修复后台文章列表链接问题

--- src/admin/controller/content.js
+++ src/admin/controller/content.js
@@ -20,7 +20,7 @@ export default class extends Base
             //文章筛选
             let map={};
             map[this.get('type')]=1;
-            articlelist = await this.model("article").join({
+            articlelist = await this.model("article").field("*,li_article.id as aid").join({
                 tags: {on: "tag, id"},
                 item: {on: ["item", "id"]},
             }).where(map).page(this.get("page"), this.get("pagesize")).order("createtime DESC").select();
@@ -29,20 +29,23 @@ export default class extends Base
         }else if(searchtxt){
             //文章搜索
             let map={title: ["like", "%"+searchtxt+"%"]};
-            articlelist = await this.model("article").join({
+            articlelist = await this.model("article").field("*,li_article.id as aid").join({
                 tags: {on: "tag, id"},
                 item: {on: ["item", "id"]},
             }).where(map).page(this.get("page"), this.get("pagesize")).order("createtime DESC").select();
             result = await this.model("article").where(map).page(this.get('page'), this.get('pagesize')).order("createtime DESC").countSelect();
             if(articlelist.length==0){
                 let map={id:searchtxt*1};
-                articlelist = await this.model("article").where(map).page(this.get("page"), this.get("pagesize")).order("createtime DESC").select();
+                articlelist = await this.model("article").field("*,li_article.id as aid").join({
+                    tags: {on: "tag, id"},
+                    item: {on: ["item", "id"]},
+                }).where(map).page(this.get("page"), this.get("pagesize")).order("createtime DESC").select();
                 result = await this.model("article").where(map).page(this.get('page'), this.get('pagesize')).order("createtime DESC").countSelect();
             }
             this.assign("type", '');
         }else{
             let map={};
-            articlelist = await this.model("article").join({
+            articlelist = await this.model("article").field("*,li_article.id as aid").join({
                 tags: {on: "tag, id"},
                 item: {on: ["item", "id"]},
             }).where(map).page(this.get("page"), this.get("pagesize")).order("createtime DESC").select();
