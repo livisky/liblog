9be6d7971706d8dfed31483c7284c51bd873e369
livisky <262248861@qq.com>
2016-10-31 10:59:30 +0800

    添加后台登陆验证码功能

--- www/static/theme/liblog/res/src/js/business/admin/login.js
+++ www/static/theme/liblog/res/src/js/business/admin/login.js
@@ -1,19 +1,3 @@
-$(function(){
-    $("#loginBtn").on('click',function(){
-        var mydata={
-            username:$("#username").val(),
-            password:$("#password").val(),
-            __CSRF__:G_csrf
-        }
-        $.ajax({
-                url:'/home/admin/dologin',
-                data:mydata,
-                type:'POST',
-                success:function(json){
-                    if(json.status===1){
-                        alert(json.msg);
-                        window.location.href="/admin/index";
-                    }else {
                         alert(json.msg);
                     }
                 }
