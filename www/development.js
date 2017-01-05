3f003d8467614f462d7746b1f57e9c4fd1a7797d
livisky <262248861@qq.com>
2017-01-05 14:43:51 +0800

    添加开发和生产运行模式

--- www/development.js
+++ www/development.js
@@ -4,19 +4,20 @@ var path = require('path');
 var rootPath = path.dirname(__dirname);
 
 var instance = new thinkjs({
-  APP_PATH: rootPath + path.sep + 'app',
-  RUNTIME_PATH: rootPath + path.sep + 'runtime',
-  ROOT_PATH: rootPath,
-  RESOURCE_PATH: __dirname,
-  UPLOAD_PATH: __dirname + "/static/upload", // 定义文件上传的目录
-  THEME_PATH:__dirname+path.sep+"static"+path.sep+"theme",
-  env: 'development'
+    APP_PATH: rootPath + path.sep + 'app',
+    RUNTIME_PATH: rootPath + path.sep + 'runtime',
+    ROOT_PATH: rootPath,
+    Dev_Src: 'src/',
+    Modules_Src: 'modules_src',
+    UPLOAD_PATH: __dirname + "/static/upload", // 定义文件上传的目录
+    THEME_PATH: __dirname + path.sep + "static" + path.sep + "theme",
+    env: 'development'
 });
 
 //compile src/ to app/
 instance.compile({
-  retainLines: true,
-  log: true
+    retainLines: true,
+    log: true
 });
 
 instance.run();
--- www/production.js
+++ www/production.js
@@ -4,13 +4,15 @@ var path = require('path');
 var rootPath = path.dirname(__dirname);
 
 var instance = new thinkjs({
-  APP_PATH: rootPath + path.sep + 'app',
-  RUNTIME_PATH: rootPath + path.sep + 'runtime',
-  ROOT_PATH: rootPath,
-  RESOURCE_PATH: __dirname,
-  UPLOAD_PATH: __dirname + "/static/upload", // 定义文件上传的目录
-  THEME_PATH:__dirname+path.sep+"static"+path.sep+"theme",
-  env: 'production'
+    APP_PATH: rootPath + path.sep + 'app',
+    RUNTIME_PATH: rootPath + path.sep + 'runtime',
+    ROOT_PATH: rootPath,
+    Dev_Src: '',
+    Modules_Src: 'modules',
+    RESOURCE_PATH: __dirname,
+    UPLOAD_PATH: __dirname + "/static/upload", // 定义文件上传的目录
+    THEME_PATH: __dirname + path.sep + "static" + path.sep + "theme",
+    env: 'production'
 });
 
 //preload packages before start server.
