d0fc497c8ac6e67b01cc8f4a684e207b730a939b
livisky <262248861@qq.com>
2017-01-05 14:46:48 +0800

    添加开发和生产运行模式

--- src/doc/controller/base.js
+++ src/doc/controller/base.js
@@ -1,51 +1,51 @@
 'use strict';
 import request from "request";
 export default class extends think.controller.base {
-  /**
-   * some base method in here
-   */
+    /**
+     * some base method in here
+     */
     async __before() {
 
         // assign后台设置
-        let _web=await this.getConfig();
-        this.assign('_web',_web);
+        let _web = await this.getConfig();
+        this.assign('_web', _web);
 
         //设置CSRF值
-        let csrf=await this.session("__CSRF__");
-        this.assign("csrf",csrf);
+        let csrf = await this.session("__CSRF__");
+        this.assign("csrf", csrf);
 
         // 是否登陆
-        let uinfo=await this.session('uInfo');
-        let islogin=(!think.isEmpty(uinfo))?1:0;
-        this.assign("islogin",islogin);
-        if(!think.isEmpty(uinfo)){
-          let logininfo=await this.model('doc').findAll('user',{name:uinfo.name});
-          //自己的个人中心基本信息
-          this.assign("logininfo",logininfo[0]);
+        let uinfo = await this.session('uInfo');
+        let islogin = (!think.isEmpty(uinfo)) ? 1 : 0;
+        this.assign("islogin", islogin);
+        if (!think.isEmpty(uinfo)) {
+            let logininfo = await this.model('doc').findAll('user', { name: uinfo.name });
+            //自己的个人中心基本信息
+            this.assign("logininfo", logininfo[0]);
         }
         //是否登陆
 
         //获取tags
-        let tagList=await this.model('doc').findAll('tags',{appear:1});
-        this.assign('tagList',tagList);
+        let tagList = await this.model('doc').findAll('tags', { appear: 1 });
+        this.assign('tagList', tagList);
 
         //获取导航链接
-        let navList=await this.model('doc').findAll('menu');
-        this.assign("navList",navList);
+        let navList = await this.model('doc').findAll('menu');
+        this.assign("navList", navList);
 
         // 设置主题地址
-        this.THEME_VIEW_PATH = `${think.THEME_PATH}${think.sep}${_web.theme}${think.sep}modules${think.sep}${this.http.module}${think.sep}`;
-        this.assign("theme_url",'static/theme/'+_web.theme+'/res');
+        this.THEME_VIEW_PATH = `${think.THEME_PATH}${think.sep}${_web.theme}${think.sep}${think.Modules_Src}${think.sep}${this.http.module}${think.sep}`;
+        this.assign("theme_url", 'static/theme/' + _web.theme + '/res');
 
     }
     async getConfig() {
-        let sysdata=await this.model('doc').findOne('system',{id:1});
-        this.assign('_web',sysdata);
+        let sysdata = await this.model('doc').findOne('system', { id: 1 });
+        this.assign('_web', sysdata);
         return sysdata;
     }
 
     // 渲染主题view层
-    async displayView(name){
-      return this.display(this.THEME_VIEW_PATH + name + '.html');
+    async displayView(name) {
+        return this.display(this.THEME_VIEW_PATH + name + '.html');
     }
 }
