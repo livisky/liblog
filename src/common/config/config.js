cef88902d85bf49cfb0ee6a95d06c7b63aaf9978
livisky <262248861@qq.com>
2017-03-24 15:10:38 +0800

    修改配置文件

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -4,22 +4,26 @@
  */
 export default {
     //key: value
-    host: "127.0.0.1", //禁止端口访问，如 http://115.29.109.30:8361；删除则不限制。
+    host: "127.0.0.1",
+    domain: "http://www.jsout.dev",
+    subdomain: {
+        bbs: "topic", //表示将 bbs.example.com 映射到 topic 模块下
+    },
     port: 8361,
     pagesize: 10,
     route_on: true,
     encoding: "utf-8",
     OSS: {
-        on: false, //true上传oss服务器，false上传应用服务器
-        domain: "", //例：http://statics.jsout.com
+        on: true, //true上传oss服务器，false上传应用服务器
+        domain: "https://statics.jsout.com",
         img_url: "static/upload/pics/",
-        region: '', //例：oss-cn-shanghai
-        accessKeyId: '', //id
-        accessKeySecret: '', //key
-        bucket: '' //jsout
+        region: 'oss-cn-shanghai',
+        accessKeyId: 'LTAIHYb49fOa6fS6',
+        accessKeySecret: 'iemKN1qFkwbz1QP1Uhho8G9oNBFcGM',
+        bucket: 'jsout'
     },
     geetest: {
-        //极验验证码配置，申请地址：http://www.geetest.com，请改成自己帐号！
+        //极验验证码配置，申请地址：http://www.geetest.com
         geetest_id: 'a4ba8061bc9741e47980e8b69e9afbf6',
         geetest_key: '4da57a23875809329871b4e0be3a53d6'
     },
